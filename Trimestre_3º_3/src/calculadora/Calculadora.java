a566bb90c5a64627ea9749cf3a71f3d62bd0a331
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-04-22 20:58:32 +0200

    entorno grafico

--- Trimestre_3º_3/src/calculadora/Calculadora.java
+++ Trimestre_3º_3/src/calculadora/Calculadora.java
@@ -52,6 +52,7 @@ public class Calculadora extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 333, 563);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(64, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
